a1f6033cf419953bba472a1b940f52021b8d1e97
Devansh <devanshsoni79@gmail.com>
2023-09-03 22:45:52 +0530

    Added sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ContactsPage from "./pages/ContactsPage";
+import ChartsAndMaps from "./pages/ChartsAndMaps";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Sidebar from "./components/Sidebar";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h2>Contacts Manager</h2>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<ContactsPage/>} />
+            <Route path="/visuals" element={<ChartsAndMaps/>} />
+          </Routes>
+        </Sidebar>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,52 @@
+import type { ReactNode } from "react";
+import { TiContacts } from "react-icons/ti/";
+import { BiMapAlt } from "react-icons/bi/";
+import { FaBars } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+type SidebarProps = {
+  children: ReactNode;
+};
+
+const menuItems = [
+  {
+    path: "/",
+    name: "Contacts",
+    icon: <TiContacts />,
+  },
+  {
+    path: "/visuals",
+    name: "Chars and Maps",
+    icon: <BiMapAlt />,
+  },
+];
+
+export default function Sidebar({ children }: SidebarProps) {
+  return (
+    <div className="flex flex-col-reverse md:flex-row h-screen">
+      <div className="bg-[#171717]">
+        {/* Menu Bars Icon */}
+        <div className="hidden">
+          <FaBars />
+        </div>
+        <div className="grid grid-flow-col md:grid-flow-row items-center">
+          {menuItems.map((item) => (
+            <NavLink to={item.path}>
+              {({ isActive, isPending }) => (
+                <div
+                  className={
+                    (isActive ? "bg-[#353535] " : "") +
+                    "flex self-center py-1 md:p-3 justify-center items-center gap-4"
+                  }
+                >
+                  <div className="text-2xl md:text-3xl">{item.icon}</div>
+                </div>
+              )}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+}
